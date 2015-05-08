95491a972005ae8e91f035f95cd2114ac6b28a41
stalincenlam <16scenlam@iaa.edu.ec>
2015-05-08 08:21:51 -0500

    Create classwork.py

---
+++ classwork.py
@@ -0,0 +1,19 @@
+mystiring = "hello" 
+myint = 20
+myfloat = 10
+
+print mystiring
+print myint
+print myfloat
+
+print "my string is" , mystiring , "my integer is" , myint , "my floating point is" , myfloat
+
+print myint + myfloat
+print myint - myfloat
+print myint * myfloat
+print myint / myfloat
+print myint % myfloat
+
+import math
+root = math.sqrt(16)
+print root
