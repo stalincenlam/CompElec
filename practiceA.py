456c6b16c241ce71e531c4359844446504f7653f
stalincenlam <16scenlam@iaa.edu.ec>
2015-05-08 08:21:24 -0500

    Create practiceA.py

---
+++ practiceA.py
@@ -0,0 +1,18 @@
+name = 'stalincenlam'
+age = 17
+gpa = 3.5
+phonenumber = 34221961
+qq = 1021149856
+
+print name
+print age 
+print gpa
+print phonenumber
+print 11
+
+
+print 'my name is', name
+print 'i am' , age , 'years old' 
+print 'my gpa is' , gpa
+print 'so here is my number' , phonenumber , 'so call me maybe' 
+print 'please also send me message in qq' , qq
