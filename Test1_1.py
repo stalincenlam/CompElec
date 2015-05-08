b8973a10f6371fd9be74abbf55cbf6ebc0c67fcf
stalincenlam <16scenlam@iaa.edu.ec>
2015-05-08 08:24:20 -0500

    Create Test1_1.py

---
+++ Test1_1.py
@@ -0,0 +1,10 @@
+operand1 = 95
+operand2 = 64.5
+
+print "95 + 64.5 =" , operand1 + operand2
+print "95 - 64.5 =" , operand1 - operand2
+print "95 * 64.5 =" , operand1 * operand2
+print "95 / 64.5 =" , operand1 / operand2
+print "95 % 64.5 =" , operand1 % operand2
+
+#My solution is best for step 16, it is because my solution were solved based on the value it was given, I will be requiring to change the value if the equation's value change.
