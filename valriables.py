f0940ccefc2cb5d800927c6114628970cba7df18
stalincenlam <16scenlam@iaa.edu.ec>
2015-05-08 08:23:46 -0500

    Create valriables.py

---
+++ valriables.py
@@ -0,0 +1,22 @@
+name = 'stalincenlam'
+age = 17
+gpa = 3.5
+
+print name
+print age 
+print gpa
+
+print 'my name is', name, '- I am' , age , 'years old' 
+
+print 'in ten years, i will be' , age + 10
+
+#Dog years
+ageInHumanYears = 3
+ageInDogYears = ageInHumanYears * 7 
+print 'my dog is' , ageInDogYears, 'years old in Dog Years.' 
+print 'But my dog is' , ageInHumanYears, 'years old in Human Years.' 
+
+myNumber = 2.5 * 5 * 2
+print myNumber
+
+# Parents, Exponents, Multiplication, Division, Addition, Subtraction
