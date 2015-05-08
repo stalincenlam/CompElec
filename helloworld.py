f763e7e877b3200124825b199a3e84bf96b8cc96
stalincenlam <16scenlam@iaa.edu.ec>
2015-05-08 08:19:59 -0500

    Create helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+print "helloworld" 
+print "my name is stalincenlam". \nThis is Python 
+print 2015 #printing an interger
+print "2015" #printing a string
+print 3.1416 #printing floating point number
+
+print 2+2
+print 2/2 
+print 2*2 
+print 2-2
