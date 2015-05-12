b098549f3171d790bbbab60d0e4494321439549a
stalincenlam <16scenlam@iaa.edu.ec>
2015-05-12 09:27:29 -0500

    Create practice2.py

---
+++ practice2.py
@@ -0,0 +1,7 @@
+import sys
+print "name" , sys.argv[1]
+print "last name" , sys.argv[2]
+print "birthday" , sys.argv[3]
+print "favorite singer" , sys.argv[4]
+print "favorite movies" , sys.argv[5]
+print "best friend" , sys.argv[6]
