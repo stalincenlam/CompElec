dba03f5bd8405dd35b77142da58ab2c031cc90bb
stalincenlam <16scenlam@iaa.edu.ec>
2015-05-22 00:14:45 -0500

    Create quiz2.py

---
+++ quiz2.py
@@ -0,0 +1,8 @@
+name = raw_input("what is your name?")
+last = raw_input("what is your last name?")
+year = input("what is your birth year?")
+subject = input("what is your class?")
+age = 2015 - year
+grade1 = input("what is your grade in S1?")
+grade2 = input("what is your grade in S2?")
+finalgrade = (grade1 + grade2) / 2 
